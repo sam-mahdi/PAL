6712aba2e0ed1c19d7ee3d56b36b385d6ee6e3ed
Sam Mahdi <65124322+sam-mahdi@users.noreply.github.com>
2022-08-16 18:10:38 -0400

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,17 @@
+if [ ! -d "$PAL_virtual_environment" ]; then 
+echo "creating virtual environment"
+python3 -m venv PAL_virtual_environment
+source PAL_virtual_environment/bin/activate
+echo "install requirements"
+pip install --upgrade pip
+pip install --upgrade setuptools
+pip install -r requirements.txt
+mv PAL PAL_virtual_environment
+cd PAL_virtual_environment
+cd PAL
+python3 PAL.py
+else
+    cd PAL_virtual_environment
+    cd PAL
+    python3 PAL.py
+fi
