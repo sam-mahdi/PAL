3c79c4acf67de1a11ae688131337b7ada767b05b
Sam Mahdi <65124322+sam-mahdi@users.noreply.github.com>
2022-08-16 18:56:39 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,21 +1,24 @@
-if [ ! -d "PAL_virtual_environment" ]; then 
-echo "creating virtual environment"
-python3 -m venv PAL_virtual_environment
-source PAL_virtual_environment/bin/activate
-echo "install requirements"
-pip install --upgrade pip
-pip install --upgrade setuptools
-pip install -r requirements.txt
-mv PAL PAL_virtual_environment
-cd PAL_virtual_environment
-cd PAL
-python3 PAL.py
-else
-    cd PAL_virtual_environment
-    cd PAL
-    if [ $1 == "no_photo" ]; then 
+if [ $1 == "no_photo" ]
+    then 
+        cd PAL
         python3 PAL.py no_photo
-    else:
-        python3 PAL.py
-    fi    
+else
+    if [ ! -d "PAL_virtual_environment" ]
+        then 
+            echo "creating virtual environment"
+            python3 -m venv PAL_virtual_environment
+            source PAL_virtual_environment/bin/activate
+            echo "install requirements"
+            pip install --upgrade pip
+            pip install --upgrade setuptools
+            pip install -r requirements.txt
+            mv PAL PAL_virtual_environment
+            cd PAL_virtual_environment
+            cd PAL
+            python3 PAL.py
+    else
+         cd PAL_virtual_environment
+         cd PAL
+         python3 PAL.py   
+    fi     
 fi
